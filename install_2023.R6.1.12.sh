410c022d36fc662e7e2166dcbb4f3d518ba1d321
veloceapps <82201681+veloceapps@users.noreply.github.com>
2024-02-14 10:33:19 +0200

    releases updated

--- install_2023.R6.1.12.sh
+++ install_2023.R6.1.12.sh
@@ -22,7 +22,7 @@ sfdx force:user:permset:assign -u $TARGET --permsetname "VeloceCPQAdmin" || echo
 sfdx force:user:permset:assign -u $TARGET --permsetname "VeloceCPQAdminReadonly" || echo "Already assigned, ignoring"
 sfdx force:user:permset:assign -u $TARGET --permsetname "VeloceCPQRuntime" || echo "Already assigned, ignoring"
 sfdx force:package:installed:list -u $TARGET | fgrep 16.7.0-8447c580-04tUc0000007d6HIAQ || sfdx force:package:install --noprompt -u $TARGET --package 16.7.0-8447c580-04tUc0000007d6HIAQ -w 15
-sfdx force:package:installed:list -u $TARGET | fgrep 16.6.0-7c16ad18-04tUc00000070GDIAY || sfdx force:package:install --noprompt -u $TARGET --package 16.6.0-7c16ad18-04tUc00000070GDIAY -w 15
+sfdx force:package:installed:list -u $TARGET | fgrep 16.6.0-8447c580-04tUc0000007d7tIAA || sfdx force:package:install --noprompt -u $TARGET --package 16.6.0-8447c580-04tUc0000007d7tIAA -w 15
 echo Installing data
 
 cat << EOF > ./sfdx-project.json
