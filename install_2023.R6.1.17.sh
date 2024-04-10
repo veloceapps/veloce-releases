0e7ee93bbce9d86ba5094b4eb3408f2a437fc61e
veloceapps <82201681+veloceapps@users.noreply.github.com>
2024-04-10 09:38:27 +0300

    releases updated

--- install_2023.R6.1.17.sh
+++ install_2023.R6.1.17.sh
@@ -17,7 +17,7 @@ then
   exit 255
 fi
 echo Installing packages
-sfdx force:package:installed:list -u $TARGET | fgrep 16.12.0-44518261-04tUc000000CObFIAW || sfdx force:package:install --noprompt -u $TARGET --package 16.12.0-44518261-04tUc000000CObFIAW -w 15
+sfdx force:package:installed:list -u $TARGET | fgrep 16.12.0-b12d28b9-04tUc000000CX8HIAW || sfdx force:package:install --noprompt -u $TARGET --package 16.12.0-b12d28b9-04tUc000000CX8HIAW -w 15
 sfdx force:user:permset:assign -u $TARGET --permsetname "VeloceCPQAdmin" || echo "Already assigned, ignoring"
 sfdx force:user:permset:assign -u $TARGET --permsetname "VeloceCPQAdminReadonly" || echo "Already assigned, ignoring"
 sfdx force:user:permset:assign -u $TARGET --permsetname "VeloceCPQRuntime" || echo "Already assigned, ignoring"
