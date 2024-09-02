5e2cf83ca347b92d29ed0da2962bac7afa21a3c6
veloceapps <82201681+veloceapps@users.noreply.github.com>
2024-09-02 11:36:06 +0300

    releases updated

--- install_latest_dev.sh
+++ install_latest_dev.sh
@@ -17,7 +17,7 @@ then
   exit 255
 fi
 echo Installing packages
-sfdx force:package:installed:list -u $TARGET | fgrep 19.1.0-0-a22e9bf4-04tUc000000JW93IAG || sfdx force:package:install --noprompt -u $TARGET --package 19.1.0-0-a22e9bf4-04tUc000000JW93IAG -w 15
+sfdx force:package:installed:list -u $TARGET | fgrep 20.0.0-2-dfa1274e-04tUc000000JckjIAC || sfdx force:package:install --noprompt -u $TARGET --package 20.0.0-2-dfa1274e-04tUc000000JckjIAC -w 15
 sfdx force:user:permset:assign -u $TARGET --permsetname "VeloceCPQAdmin" || echo "Already assigned, ignoring"
 sfdx force:user:permset:assign -u $TARGET --permsetname "VeloceCPQAdminReadonly" || echo "Already assigned, ignoring"
 sfdx force:user:permset:assign -u $TARGET --permsetname "VeloceCPQRuntime" || echo "Already assigned, ignoring"
