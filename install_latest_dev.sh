b0353a32474b1abf925b829d2c791a0b27290b40
veloceapps <82201681+veloceapps@users.noreply.github.com>
2024-12-11 18:19:55 +0500

    releases updated

--- install_latest_dev.sh
+++ install_latest_dev.sh
@@ -21,7 +21,7 @@ sfdx force:package:installed:list -u $TARGET | fgrep 21.0.0-2-9a5d4013-04tUc0000
 sfdx force:user:permset:assign -u $TARGET --permsetname "VeloceCPQAdmin" || echo "Already assigned, ignoring"
 sfdx force:user:permset:assign -u $TARGET --permsetname "VeloceCPQAdminReadonly" || echo "Already assigned, ignoring"
 sfdx force:user:permset:assign -u $TARGET --permsetname "VeloceCPQRuntime" || echo "Already assigned, ignoring"
-sfdx force:package:installed:list -u $TARGET | fgrep 21.0.0-2-9a5d4013-04tUc000000NrV3IAK || sfdx force:package:install --noprompt -u $TARGET --package 21.0.0-2-9a5d4013-04tUc000000NrV3IAK -w 15
+sfdx force:package:installed:list -u $TARGET | fgrep 21.0.0-3-9a5d4013-04tUc000000NrYHIA0 || sfdx force:package:install --noprompt -u $TARGET --package 21.0.0-3-9a5d4013-04tUc000000NrYHIA0 -w 15
 sfdx force:package:installed:list -u $TARGET | fgrep 21.0.0-0-9a5d4013-04tUc000000NrZtIAK || sfdx force:package:install --noprompt -u $TARGET --package 21.0.0-0-9a5d4013-04tUc000000NrZtIAK -w 15
 echo Installing data
 
