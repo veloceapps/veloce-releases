d6f7a9788969d97b1bbd0ea5d5eedf514cfac558
veloceapps <82201681+veloceapps@users.noreply.github.com>
2025-05-13 17:38:13 +0300

    releases updated

--- install_2024.R7.5.2.sh
+++ install_2024.R7.5.2.sh
@@ -22,7 +22,7 @@ sfdx force:user:permset:assign -u $TARGET --permsetname "VeloceCPQAdmin" || echo
 sfdx force:user:permset:assign -u $TARGET --permsetname "VeloceCPQAdminReadonly" || echo "Already assigned, ignoring"
 sfdx force:user:permset:assign -u $TARGET --permsetname "VeloceCPQRuntime" || echo "Already assigned, ignoring"
 sfdx force:package:installed:list -u $TARGET | fgrep 20.2.0-613b89d5-04tUc000000ZY6rIAG || sfdx force:package:install --noprompt -u $TARGET --package 20.2.0-613b89d5-04tUc000000ZY6rIAG -w 15
-sfdx force:package:installed:list -u $TARGET | fgrep 20.2.0-3479f55a-04tUc000000ZXnVIAW || sfdx force:package:install --noprompt -u $TARGET --package 20.2.0-3479f55a-04tUc000000ZXnVIAW -w 15
+sfdx force:package:installed:list -u $TARGET | fgrep 20.2.0-613b89d5-04tUc000000ZYEvIAO || sfdx force:package:install --noprompt -u $TARGET --package 20.2.0-613b89d5-04tUc000000ZYEvIAO -w 15
 echo Installing data
 
 cat << EOF > ./sfdx-project.json
